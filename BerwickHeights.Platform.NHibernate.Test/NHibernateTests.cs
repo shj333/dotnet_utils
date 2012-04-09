55dcb52bea5b6c1fcac900a0f7c15dea8bf1e1ec
stuart <stuart@ba65fcbd-5449-444a-b89d-35d1248ec886>
2012-04-09 11:20:06 +0000

    Added NHibernate.Test project

---
+++ BerwickHeights.Platform.NHibernate.Test/NHibernateTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Transactions;
+using BerwickHeights.Platform.IoC;
+using Castle.Services.Transaction;
+using FluentNHibernate;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Conventions.Helpers;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using NHCfg = NHibernate.Cfg;
+using NUnit.Framework;
+using ILoggerFactory = BerwickHeights.Platform.Logging;
+using ITransaction = NHibernate.ITransaction;
+using Log4NetLoggerFactory = BerwickHeights.Platform.Logging.Log4Net.Log4NetLoggerFactory;
+
+namespace BerwickHeights.Platform.NHibernate.Test
+{
+    [TestFixture]
+    public class NHibernateTests
+    {
+        private IIoCContainerManager iocContainer;
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            iocContainer = IoCContainerManagerFactory.GetIoCContainerManager();
+            iocContainer.RegisterLoggerFactory(new Log4NetLoggerFactory());
+
+            iocContainer.SetupNHibernateIntegration(ConfigureDatabase(), ConfigureMappings(), ExposeConfigAction, false, true);
+        }
+
+        [Test]
+        [Transaction(TransactionScopeOption.Required)]
+        public void TestPersistEntity()
+        {
+            TestEntity entity = new TestEntity(13, "data2");
+            Console.WriteLine("TestEntityId: " + entity.TestEntityId);
+            using (ISession session = iocContainer.Resolve<ISession>())
+            {
+//                using (ITransaction trans = session.BeginTransaction())
+ ///               {
+                    session.SaveOrUpdate(entity);
+      //              trans.Commit();
+    //            }
+            }
+        }
+
+        private static IPersistenceConfigurer ConfigureDatabase()
+        {
+            return
+                MsSqlConfiguration.MsSql2008.ConnectionString(x => x.FromConnectionStringWithKey("TestDatabase")).ShowSql();
+        }
+
+        private static AutoPersistenceModel ConfigureMappings()
+        {
+            return AutoMap
+                .AssemblyOf<TestEntity>(new AutomapConfig())
+                .Conventions.Add(Table.Is(x => x.EntityType.Name));
+        }
+
+        private static void ExposeConfigAction(NHCfg.Configuration config)
+        {
+            SchemaMetadataUpdater.QuoteTableAndColumns(config);
+            //new SchemaExport(config).Create(false, true);
+        }
+
+        /// <summary>
+        /// Configuration for FluentNHibernate AutoMapper
+        /// </summary>
+        public class AutomapConfig : DefaultAutomappingConfiguration
+        {
+            /// <summary>
+            /// Specifies the namespace where DTOs live
+            /// </summary>
+            public override bool ShouldMap(Type type)
+            {
+                return (type == typeof(TestEntity));
+            }
+
+            /// <summary>
+            /// Determines whether or not the given DTO member is the identity.
+            /// </summary>
+            public override bool IsId(Member member)
+            {
+                return (member.Name.Equals(member.DeclaringType.Name + "Id"));
+            }
+        }
+    }
+}
---
+++ BerwickHeights.Platform.NHibernate.Test/TestEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BerwickHeights.Platform.NHibernate.Test
+{
+    public class TestEntity
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+        /// </summary>
+        public TestEntity()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+        /// </summary>
+        public TestEntity(int data1, string data2)
+        {
+            TestEntityId = Guid.NewGuid().ToString();
+            Data1 = data1;
+            Data2 = data2;
+        }
+
+        public virtual string TestEntityId { get; protected internal set; }
+
+        public virtual int Data1 { get; protected internal set; }
+
+        public virtual string Data2 { get; protected internal set; }
+    }
+}
