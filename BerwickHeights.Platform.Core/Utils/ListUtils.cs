c8112d234f097507645f9d21b884c0c71c8640fa
stuart <stuart@ba65fcbd-5449-444a-b89d-35d1248ec886>
2012-05-01 20:53:06 +0000

    Added ListUtils to Core

---
+++ BerwickHeights.Platform.Core/Utils/ListUtils.cs
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2012 Berwick Heights Software, Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License. You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is 
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and limitations under the License.
+ *  
+ */
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace BerwickHeights.Platform.Core.Utils
+{
+    /// <summary>
+    /// Static utility methods for managing lists.
+    /// </summary>
+    public static class ListUtils
+    {
+        /// <summary>
+        /// Returns a string version of the given list as "[item1,item2,item3,...]".
+        /// </summary>
+        public static string ListToString(IEnumerable<object> list)
+        {
+            StringBuilder sb = new StringBuilder("[");
+            foreach (object item in list) sb.Append(item + ",");
+            sb.Append("]");
+            return sb.ToString();
+        }
+    }
+}
